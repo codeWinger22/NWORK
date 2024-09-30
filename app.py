87f74d040c41d21622c11f96dbf13db6ea7c6a31
codeWinger22 <gourivpawar@gmail.com>
2024-09-30 10:52:13 +0530

    hell

--- app.py
+++ app.py
@@ -45,10 +45,10 @@ def form():
 @app.route('/submit', methods=['POST'])
 def submit():
     call_time = request.form['call_time']  # Get the call time from the form
+    
+    dish = request.form['dish']              # Get the favorite dish from the form
     print(call_time)
     print(dish)
-    dish = request.form['dish']              # Get the favorite dish from the form
-
     # Append the data to a text file
     with open('submissions.txt', 'a') as f:
         f.write(f'Call Time: {call_time}, Favorite Dish: {dish}\n')
